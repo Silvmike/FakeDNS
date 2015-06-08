f0da56e01c78d84ce5a1b393c024673c730287c2
Michael Silvanovich <silvanovich.michael@gmail.com>
2015-06-08 16:31:30 +0300

    [fakedns][ added netcat based client ]

---
+++ fakedns-client.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+  echo "You must specify 3 args: registrator hostname, registrator port, your fake host name!"
+  exit 
+fi
+
+echo "$3\n" | netcat $1 $2
+
+echo OK
